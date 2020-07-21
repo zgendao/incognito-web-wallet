7cb2cb7945833ef6bd4033076cf73b20abc55763
Domokos Vali <vali.domokos@gmail.com>
2020-07-21 03:32:13 +0200

    in progress

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -2,7 +2,8 @@
   (:require [reagent.core :as reagent :refer [atom create-class dom-node]]
             ["qrcode" :as qrcode]
             [app.state :refer [state]]
-            [app.api :refer [wallet]]))
+            [app.api :refer [wallet]]
+            [animate-css-grid :refer [wrapGrid]]))
 
 (defn qr-code [text]
   (create-class
@@ -14,6 +15,39 @@
     :reagent-render (fn []
                       [:canvas#canvas])}))
 
+(defn navbar []
+  [:nav
+   [:div.container
+    [:div.navbar__brand
+      [:img {:src "./images/logo.png" :width "30px" :height "30px"}]
+      [:p "Incognito Web Wallet"]]
+    [:div
+      [:p (:prv-price @state) " USD"]]]])
+
+(defn account-selector [name balance]
+  [:div.account-selector
+    [:h6.account-selector__name name]
+    [:div.account-selector__balance
+      [:p balance]]])
+
+(defn accounts-grid []
+  (create-class
+    {:component-did-mount
+      (fn []
+        (do
+          (def grid (.querySelector js/document ".js-grid"))
+          (wrapGrid grid #js {:duration 600})))
+     :reagent-render
+      (fn []
+        [:div.accounts-wrapper.js-grid
+          (repeat 10
+            [account-selector "Wallet 1" "300"])])}))
+
+(defn accounts []
+  [:div.container
+    [:div#accounts
+      [accounts-grid]]])
+
 (defn wallet-ui []
   (create-class
    {:component-did-mount
@@ -24,8 +58,7 @@
     :reagent-render
     (fn []
       (let [account (first (js->clj (:accounts @state)))]
-        [:div {:style {:display "flex" :justify-content "center" :align-items "center" :flex-direction "column" :padding-bottom "60px" :padding-top "30px"}}
-         [:h3 "Your Web Wallet:"]
+        [:div.container
          (when account
            [:div
             [:h6 "Name: " (-> account .-name)]
@@ -42,8 +75,9 @@
              [:input {:type "text" :id "validator" :value (-> account .-nativeToken .-accountKeySet .-validatorKey)}]]])]))}))
 
 (defn app []
-  [:div {:style {:display "flex" :justify-content "center" :align-items "center" :flex-direction "column"}}
-   [:h1 (:prv-price @state)]
-   (if (:wasm-loaded @state)
-     [wallet-ui]
-     [:h1 "Loading.."])])
+  (if (:wasm-loaded @state)
+    [:<>
+      [navbar]
+      [accounts]
+      [wallet-ui]]
+    [:h1 "Loading.."]))
